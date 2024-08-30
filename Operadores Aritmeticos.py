fbcee9eb3526889745af859ea6bc196c3a554368
Kevin Candela <Kevadriesp@gmail.com>
2024-08-30 17:09:56 -0500

    Ejercicio 7

--- Operadores Aritmeticos.py
+++ Operadores Aritmeticos.py
@@ -25,5 +25,13 @@ Ejemplo5 = 15/3*(7+(68-15*33+(math.pow(45,2)/5)/3)/2)+19
 #Imprimir variable 
 print(f"El resultado de la operacion es:{Ejemplo5}")
 
+#Ejemplo IIIIII
+Ejemplo6 = 15/3*(7+(68-15*33+(math.pow(45,2)/5)/3)/2)+19
+#Imprimir variable 
+print(f"El resultado de la operacion es:{Ejemplo6}")
 
+#Ejemplo IIIIIII
+Ejemplo7 =(16/4*((((4+8*2)/4- (3**2)+math.sqrt(4))/2)*math.sqrt(25))-3)+16
+#Imprimir variable 
+print(f"El resultado de la operacion es:{Ejemplo7}")
 
