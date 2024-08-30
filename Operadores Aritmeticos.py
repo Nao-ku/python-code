c686c50d696faa38d0e1ab5ca5561c51d821e7f4
Kevin Candela <Kevadriesp@gmail.com>
2024-08-30 16:10:56 -0500

    first comiit

--- Operadores Aritmeticos.py
+++ Operadores Aritmeticos.py
@@ -6,11 +6,18 @@ Ejemplo1 = 5*(2+5)
 print(f"El resultado de la operacion es:{Ejemplo1}")
 
 #Ejemplo II
-Ejemplo1 = (4+3)-((3*2)+1)
+Ejemplo2 = (4+3)-((3*2)+1)
 #Imprimir variable 
-print(f"El resultado de la operacion es:{Ejemplo1}")
+print(f"El resultado de la operacion es:{Ejemplo2}")
 
 #Ejemplo III
-Ejemplo1 = 2*3+5(9/3)
+Ejemplo3 = 2*(3+(5*(9/3)))
 #Imprimir variable 
-print(f"El resultado de la operacion es:{Ejemplo1}")
+print(f"El resultado de la operacion es:{Ejemplo3}")
+
+#Ejemplo IIII
+Ejemplo4 = 7+3*(1+5*(2*(9-7)+4)+2)
+#Imprimir variable 
+print(f"El resultado de la operacion es:{Ejemplo4}")
+
+
