4ebf5ceab0a1a70e12dc23209a308df53b59347c
Naoku <kevadriesp@gmail.com>
2024-08-30 15:33:03 -0500

    Aritmetica

---
+++ Operadores Aritmeticos.py
@@ -0,0 +1,11 @@
+#importar libreria
+import math 
+#Ejemplo I
+Ejemplo1 = 5*(2+5)
+#Imprimir variable 
+print(f"El resultado de la operacion es:{Ejemplo1}")
+
+#Ejemplo II
+Ejemplo1 = (4+3)-((3*2)+1)
+#Imprimir variable 
+print(f"El resultado de la operacion es:{Ejemplo1}")
